0a5daa2f7e7a36ea9226a0f89579b8c9fe3caab5
haripriyamanne <haripriyamanne4@gmail.com>
2021-07-09 21:14:57 +0530

    Add project files.

---
+++ Migrations/20210709132211_Coloumns Added.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Asp.netCoreMvcCrud.Migrations
+{
+    public partial class ColoumnsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    CompanyType = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    CompanyLimited = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    CompanyWebsite = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Busineess = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Benifits = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    ListProducts = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    TrustElements = table.Column<string>(type: "nvarchar(250)", maxLength: 100, nullable: false),
+                    ImportantAreas = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    WebsiteExample = table.Column<string>(type: "nvarchar(250)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,100 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asp.netCoreMvcCrud.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage = "FirstName is mandatory")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "LastName is mandatory")]
+        public string LastName { get; set; }
+        //[RegularExpression("^[a-z0-9][@][g][m][a][i][l][.][c][o][m]$")]
+        [Required(ErrorMessage = "Email is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        [Column(TypeName = "nvarchar(10)")]
+        //[RegularExpression("^[6-9]{1}[0-9]{9}$")]
+        [Required(ErrorMessage = "Phone Number is mandatory")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Gender is mandatory")]
+        [Column(TypeName = "nvarchar(10)")]
+        public Gender Gender { get; set; }
+        [Display(Name = "Company Name")]
+        [Required(ErrorMessage = "Company Name is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string CompanyName { get; set; }
+        [Display(Name = "Company Type")]
+        [Required(ErrorMessage = "Company Type is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public CompanyType CompanyType { get; set; }
+        [Display(Name = "Company Limited")]
+        [Required(ErrorMessage = "Company Limited is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string CompanyLimited { get; set; }
+        [Display(Name = "Company Website")]
+        [Required(ErrorMessage = "Company Website is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string CompanyWebsite { get; set; }
+        [Display(Name = "Please Describe Your Business")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Busineess { get; set; }
+        [Display(Name = "Please Provide 3-7 Benefits that you offers to your Customers")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Benifits { get; set; }
+        [Display(Name = "Please List the Products or Services you want to showcase your HomePage")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string ListProducts { get; set; }
+        [Display(Name = "Add 1-3 Trust Elements")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        [StringLength(100, MinimumLength = 3)]
+        public string TrustElements { get; set; }
+        //[Display(Name = "List any Service areas you want to target")]
+        //[Required(ErrorMessage = "This field is mandatory")]
+        //[Column(TypeName = "nvarchar(250)")]
+        //public List<CheckBoxModel> ServiceAreas { get; set; }
+        [Display(Name = "List Anything else you would like to mention or feel is important")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string ImportantAreas { get; set; }
+        [Display(Name = "List one website example that you like and what you like about them ?")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string WebsiteExample { get; set; }
+
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public enum CompanyType
+    {
+        Manfacturing,
+        Industry,
+        Software,
+        NGO,
+        OutSourcing
+    }
+    //public enum ServiceAreas
+    //{
+    //    Cleaning,
+    //    Software,
+    //    UserInterface,
+    //    Product,
+    //    Backend
+    //}
+
+}
